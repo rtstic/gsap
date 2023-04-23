51d489675375f16e07b757b33749528b5d00acdf
Raimon T Simon <raimonrts@gmail.com>
2023-04-23 23:47:16 +0530

    added title animations

--- swiper-clone.js
+++ swiper-clone.js
@@ -42,12 +42,91 @@ matchLetterPosition(e1Static, e1);
 matchLetterPosition(s1Static, s1);
 
 /* title block TWO */
+const title2Static = document.querySelector("#title-2-static");//for text animation inital position
+const title3Animated = document.querySelector("#title-3-animated");//for text animation final position
+const title3Nospan = document.querySelector("#title-3-nospan");//for fade in animation
+//split spans
+splitSpans(title2Static, "title2static");
+splitSpans(title3Animated, "title3animated");
+//initial position
+const m2Static = document.querySelector("#m2-static");
+const i2Static = document.querySelector("#i2-static");
+const o2Static = document.querySelector("#o2-static");
+const t2Static = document.querySelector("#t2-static");
+const a2Static = document.querySelector("#a2-static");
+const e2Static = document.querySelector("#e2-static");
+const s2Static = document.querySelector("#s2-static");
+//final position 
+const m3Animated = document.querySelector("#m3-animated");
+const i3Animated = document.querySelector("#i3-animated");
+const o3Animated = document.querySelector("#o3-animated");
+const t3Animated = document.querySelector("#t3-animated");
+const a3Animated = document.querySelector("#a3-animated");
+const e3Animated = document.querySelector("#e3-animated");
+const s3Animated = document.querySelector("#s3-animated");
+//animated letters
+const m2 = document.querySelector("#m2");
+const i2 = document.querySelector("#i2");
+const o2= document.querySelector("#o2");
+const t2 = document.querySelector("#t2");
+const a2 = document.querySelector("#a2");
+const e2= document.querySelector("#e2");
+const s2 = document.querySelector("#s2");
+//match initail letter position
+matchLetterPosition(m2Static, m2);
+matchLetterPosition(i2Static, i2);
+matchLetterPosition(o2Static, o2);
+matchLetterPosition(t2Static, t2);
+matchLetterPosition(a2Static, a2);
+matchLetterPosition(e2Static, e2);
+matchLetterPosition(s2Static, s2);
+
+//title block 3
+const title3Static = document.querySelector("#title-3-static");//for text animation inital position
+const title4Animated = document.querySelector("#title-4-animated");//for text animation final position
+const title4Nospan = document.querySelector("#title-4-nospan");//for fade in animation
+//split spans
+splitSpans(title3Static, "title3static");
+splitSpans(title4Animated, "title4animated");
+//initial position
+const m3Static = document.querySelector("#m3-static");
+const i3Static = document.querySelector("#i3-static");
+const o3Static = document.querySelector("#o3-static");
+const t3Static = document.querySelector("#t3-static");
+const a3Static = document.querySelector("#a3-static");
+const e3Static = document.querySelector("#e3-static");
+const s3Static = document.querySelector("#s3-static");
+//final position 
+const m4Animated = document.querySelector("#m4-animated");
+const i4Animated = document.querySelector("#i4-animated");
+const o4Animated = document.querySelector("#o4-animated");
+const t4Animated = document.querySelector("#t4-animated");
+const a4Animated = document.querySelector("#a4-animated");
+const e4Animated = document.querySelector("#e4-animated");
+const s4Animated = document.querySelector("#s4-animated");
+//animated letters
+const m3 = document.querySelector("#m3");
+const i3 = document.querySelector("#i3");
+const o3= document.querySelector("#o3");
+const t3 = document.querySelector("#t3");
+const a3 = document.querySelector("#a3");
+const e3= document.querySelector("#e3");
+const s3 = document.querySelector("#s3");
+//match initail letter position
+matchLetterPosition(m3Static, m3);
+matchLetterPosition(i3Static, i3);
+matchLetterPosition(o3Static, o3);
+matchLetterPosition(t3Static, t3);
+matchLetterPosition(a3Static, a3);
+matchLetterPosition(e3Static, e3);
+matchLetterPosition(s3Static, s3);
 
 
 //initialize on page load
 gsap.set(".title-block-0", { opacity: 1 });
 gsap.set(".title-block-1", { opacity: 0 });
-gsap.set(".title-block-1", { opacity: 0 });
+gsap.set(".title-block-2", { opacity: 0 });
+gsap.set(".title-block-3", { opacity: 0 });
 gsap.set(".animated-letters-1", { opacity: 0 });
 gsap.set(".animated-letters-2", { opacity: 0 });
 gsap.set(".animated-letters-3", { opacity: 0 });
@@ -96,17 +175,25 @@ document.addEventListener("DOMContentLoaded", () => {
           }
         }
         if (activeIndex === 2) {
+          gsap.to(".title-block-2", { opacity: 1 });
+          gsap.to(".title-block-1", { opacity: 0 });
           if (visitedTwo) {
+            title3fadein();
             console.log("perfrom fade in for title three");
           } else {
             console.log("perfrom text animation to reveal title three");
+            titile3TextAnimation();
             visitedTwo = true;
           }
         }
         if (activeIndex === 3) {
+          gsap.to(".title-block-3", { opacity: 1 });
+          gsap.to(".title-block-2", { opacity: 0 });
           if (visitedThree) {
+            title4fadein()
             console.log("perform fade in for title four");
           } else {
+            titile4TextAnimation()
             console.log("perfrom text animation to reveal title four");
             visitedThree = true;
           }
@@ -190,6 +277,7 @@ allZero();
 function title2fadein() {
   allZero();
   gsap.to([title2Nospan], { opacity: 1, duration: 1 ,delay:0.5});
+  gsap.set(".animated-letters-2", { opacity: 0 });
 }
 //title2 text animation
 function titile2TextAnimation() {
@@ -221,12 +309,88 @@ gsap.to([title1Nospan], { opacity: 0});
   gsap.to([title2Nospan], { opacity: 1 ,duration:3 ,delay:5});
   gsap.to(".animated-letters-1", { opacity: 0 ,delay:8});
 }
+//title3 fadein
+function title3fadein() {
+  allZero();
+  gsap.to([title3Nospan], { opacity: 1, duration: 1 ,delay:0.5});
+}
+//title3 text animation
+function titile3TextAnimation() {
+  //initialization
+  gsap.to([title2Nospan], { opacity: 0});
+    gsap.to([title2Static], { opacity: 1});
+    gsap.set([title3Animated], { opacity: 0 });
+    gsap.set([title3Nospan], { opacity: 0 });
+    gsap.set(".animated-letters-2", { opacity: 1 });
+    //animation starts
+    gsap.to([title2Static], { opacity: 0 ,duration:1,delay:1});
+  //animate x positions
+    gsap.to([m2],{ duration: 2, ...moveX(m3Animated, m2),ease: Circ.easeOut,delay:2});
+    gsap.to([i2],{ duration: 2, ...moveX(i3Animated, i2),ease: Circ.easeOut,delay:2});
+    gsap.to([o2],{ duration: 2, ...moveX(o3Animated, o2),ease: Circ.easeOut,delay:2});
+    gsap.to([t2],{ duration: 2, ...moveX(t3Animated, t2),ease: Circ.easeOut,delay:2});
+    gsap.to([a2],{ duration: 2, ...moveX(a3Animated, a2),ease: Circ.easeOut,delay:2});
+    gsap.to([e2],{ duration: 2, ...moveX(e3Animated, e2),ease: Circ.easeOut,delay:2});
+    gsap.to([s2],{ duration: 2, ...moveX(s3Animated, s2),ease: Circ.easeOut,delay:2});
+  //animate y positions
+    gsap.to([m2],{ duration: 1, ...moveY(m3Animated, m2),ease: Circ.easeOut,delay:4});
+    gsap.to([i2],{ duration: 1, ...moveY(i3Animated, i2),ease: Circ.easeOut,delay:4});
+    gsap.to([o2],{ duration: 1, ...moveY(o3Animated, o2),ease: Circ.easeOut,delay:4});
+    gsap.to([t2],{ duration: 1, ...moveY(t3Animated, t2),ease: Circ.easeOut,delay:4});
+    gsap.to([a2],{ duration: 1, ...moveY(a3Animated, a2),ease: Circ.easeOut,delay:4});
+    gsap.to([e2],{ duration: 1, ...moveY(e3Animated, e2),ease: Circ.easeOut,delay:4});
+    gsap.to([s2],{ duration: 1, ...moveY(s3Animated, s2),ease: Circ.easeOut,delay:4});
+  //play lottie along with y movement
+    gsap.to([title3Nospan], { opacity: 1 ,duration:3 ,delay:5});
+    gsap.to(".animated-letters-2", { opacity: 0 ,delay:8});
+  }
+  //title4 fadein
+function title4fadein() {
+  allZero();
+  gsap.to([title4Nospan], { opacity: 1, duration: 1 ,delay:0.5});
+}
+//title4 text animation
+function titile4TextAnimation() {
+  //initialization
+  gsap.to([title3Nospan], { opacity: 0});
+    gsap.to([title3Static], { opacity: 1});
+    gsap.set([title4Animated], { opacity: 0 });
+    gsap.set([title4Nospan], { opacity: 0 });
+    gsap.set(".animated-letters-3", { opacity: 1 });
+    //animation starts
+    gsap.to([title3Static], { opacity: 0 ,duration:1,delay:1});
+  //animate x positions
+    gsap.to([m3],{ duration: 2, ...moveX(m4Animated, m3),ease: Circ.easeOut,delay:2});
+    gsap.to([i3],{ duration: 2, ...moveX(i4Animated, i3),ease: Circ.easeOut,delay:2});
+    gsap.to([o3],{ duration: 2, ...moveX(o4Animated, o3),ease: Circ.easeOut,delay:2});
+    gsap.to([t3],{ duration: 2, ...moveX(t4Animated, t3),ease: Circ.easeOut,delay:2});
+    gsap.to([a3],{ duration: 2, ...moveX(a4Animated, a3),ease: Circ.easeOut,delay:2});
+    gsap.to([e3],{ duration: 2, ...moveX(e4Animated, e3),ease: Circ.easeOut,delay:2});
+    gsap.to([s3],{ duration: 2, ...moveX(s4Animated, s3),ease: Circ.easeOut,delay:2});
+  //animate y positions
+    gsap.to([m3],{ duration: 1, ...moveY(m4Animated, m3),ease: Circ.easeOut,delay:4});
+    gsap.to([i3],{ duration: 1, ...moveY(i4Animated, i3),ease: Circ.easeOut,delay:4});
+    gsap.to([o3],{ duration: 1, ...moveY(o4Animated, o3),ease: Circ.easeOut,delay:4});
+    gsap.to([t3],{ duration: 1, ...moveY(t4Animated, t3),ease: Circ.easeOut,delay:4});
+    gsap.to([a3],{ duration: 1, ...moveY(a4Animated, a3),ease: Circ.easeOut,delay:4});
+    gsap.to([e3],{ duration: 1, ...moveY(e4Animated, e3),ease: Circ.easeOut,delay:4});
+    gsap.to([s3],{ duration: 1, ...moveY(s4Animated, s3),ease: Circ.easeOut,delay:4});
+  //play lottie along with y movement
+    gsap.to([title4Nospan], { opacity: 1 ,duration:3 ,delay:5});
+    gsap.to(".animated-letters-3", { opacity: 0 ,delay:8});
+  }
 
 function allZero(){
     gsap.to([title1Static], { opacity: 0 });
     gsap.to([title2Animated], { opacity: 0 });
     gsap.to([title1Nospan], { opacity: 0 });
     gsap.to([title2Nospan], { opacity: 0});
+    gsap.to([title3Nospan], { opacity: 0});
+    gsap.to([title2Static], { opacity: 0});
+    gsap.to([title3Animated], { opacity: 0});
+    gsap.to([title4Nospan], { opacity: 0});
+    gsap.to([title3Static], { opacity: 0});
+    gsap.to([title4Animated], { opacity: 0});
 }
 
 window.addEventListener("DOMContentLoaded", function () {
@@ -236,4 +400,5 @@ window.addEventListener("DOMContentLoaded", function () {
 
       }, 5000))();
   });
-  
+
+  console.log('object');
