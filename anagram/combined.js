b3b543d6e6ace7103c4aa31ee207ef34cc6a7dce
Raimon T Simon <raimonrts@gmail.com>
2023-05-05 00:09:54 +0530

    body overflow

--- anagram/combined.js
+++ anagram/combined.js
@@ -215,7 +215,7 @@ function matchLetterPosition(staticLetter, animatedLetter) {
           init2();
         createTimeline3();
         }
-        document.body.style.overflow = 'auto';
+       
       }, 2500))();
   });
 
